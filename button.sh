31e6e648cbca399dbdfe9370d6327e8b194899c9
Adam-Kruger <odpszkolapd@gmail.com>
2021-04-12 18:28:54 +0000

    Reaguje

--- button.sh
+++ button.sh
@@ -4,8 +4,8 @@ echo "in" > sys/class/gpio/gpio2/direction
 
 while [ 1 ]
 do
-    if [sys/class/gpio/gpio2/value == 1]
+    if [$(cat /sys/class/gpio/gpio2/value) == 1]
     then
         echo "Przycisk nacisniety"
     fi
-done    
+done  
