6dafda53b61a90141f8fae9509dd4e84e80c8d9c
Adam-Kruger <odpszkolapd@gmail.com>
2021-04-13 08:19:17 +0000

    Working

---
+++ ledplus.cpp
@@ -0,0 +1,18 @@
+#include<wiringPi.h>
+
+using namespace std;
+
+int main()
+{
+    wiringPiSetupGpio();
+    pinMode(2, OUTPUT);
+
+    for(;;)
+    {
+        digitalWrite(2, HIGH);
+        delay(1000);
+        digitalWrite(2, LOW);
+        delay(1000);
+    }
+    return 0;
+}
